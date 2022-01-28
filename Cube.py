66a117f428d9cd82ef48cd6e295cf4939235204c
NeuralNacho <anguswhittam@btinternet.com>
2022-01-28 16:44:01 +0000

    init

---
+++ Cube.py
@@ -0,0 +1,109 @@
+import numpy as np
+
+class Cube:
+    def __init__(self, n):
+        self.dim = n
+        face = np.ones((n,n), dtype = int)
+        # viewing each face so that unfolds into hotizontal t shape like so: -|--
+        self.up_face = np.multiply(face, 0)
+        self.down_face = np.multiply(face, 1)
+        self.front_face = np.multiply(face, 2)
+        self.back_face = np.multiply(face, 3) 
+        self.right_face = np.multiply(face, 4)
+        self.left_face = np.multiply(face, 5)
+
+    def clockwise_right(self):
+        self.right_face = np.rot90(self.right_face, -1)
+        temp = self.up_face[:,-1].copy()  # gives last column
+        self.up_face[:,-1] = self.front_face[:,-1]
+        self.front_face[:,-1] = self.down_face[:,0][::-1]  # [::-1] reverses array
+        self.down_face[:,0] = self.back_face[:,-1][::-1]
+        self.back_face[:,-1] = temp
+
+    def clockwise_left(self):
+        self.left_face = np.rot90(self.left_face, -1)
+        temp = self.up_face[:,0].copy()
+        self.up_face[:,0] = self.back_face[:,0]
+        self.back_face[:,0] = self.down_face[:,-1][::-1]
+        self.down_face[:,-1] = self.front_face[:,0][::-1]
+        self.front_face[:,0] = temp
+
+    def clockwise_front(self):
+        self.front_face = np.rot90(self.front_face, -1)
+        temp = self.up_face[-1].copy()
+        self.up_face[-1] = self.left_face[-1]
+        self.left_face[-1] = self.down_face[-1]
+        self.down_face[-1] = self.right_face[-1]
+        self.right_face[-1] = temp
+
+    def clockwise_back(self):
+        self.back_face = np.rot90(self.back_face, -1)
+        temp = self.up_face[0].copy()
+        self.up_face[0] = self.right_face[0]
+        self.right_face[0] = self.down_face[0]
+        self.down_face[0] = self.left_face[0]
+        self.left_face[0] = temp
+
+    def clockwise_up(self):
+        self.up_face = np.rot90(self.up_face, -1)
+        temp = self.back_face[-1].copy()
+        self.back_face[-1] = self.left_face[:,-1][::-1]
+        self.left_face[:,-1] = self.front_face[0]
+        self.front_face[0] = self.right_face[:,0][::-1]
+        self.right_face[:,0] = temp
+
+    def clockwise_down(self):
+        self.down_face = np.rot90(self.down_face, -1)  # checked in my head that this is correct
+        temp = self.back_face[0].copy()
+        self.back_face[0] = self.right_face[:,-1]
+        self.right_face[:,-1] = self.front_face[-1][::-1]
+        self.front_face[-1] = self.left_face[:,0]
+        self.left_face[:,0] = temp[::-1]
+    
+    def anticlockwise_right(self):
+        self.right_face = np.rot90(self.right_face)
+        temp = self.up_face[:,-1].copy()  # gives last column
+        self.up_face[:,-1] = self.back_face[:,-1]
+        self.back_face[:,-1] = self.down_face[:,0][::-1]
+        self.down_face[:,0] = self.front_face[:,-1][::-1]
+        self.front_face[:,-1] = temp
+
+    def anticlockwise_left(self):
+        self.left_face = np.rot90(self.left_face)
+        temp = self.up_face[:,0].copy()
+        self.up_face[:,0] = self.front_face[:,0]
+        self.front_face[:,0] = self.down_face[:,-1][::-1]
+        self.down_face[:,-1] = self.back_face[:,0][::-1]
+        self.back_face[:,0] = temp
+
+    def anticlockwise_front(self):
+        self.front_face = np.rot90(self.front_face)
+        temp = self.up_face[-1].copy()
+        self.up_face[-1] = self.right_face[-1]
+        self.right_face[-1] = self.down_face[-1]
+        self.down_face[-1] = self.left_face[-1]
+        self.left_face[-1] = temp
+
+    def anticlockwise_back(self):
+        self.back_face = np.rot90(self.back_face)
+        temp = self.up_face[0].copy()
+        self.up_face[0] = self.left_face[0]
+        self.left_face[0] = self.down_face[0]
+        self.down_face[0] = self.right_face[0]
+        self.right_face[0] = temp
+
+    def anticlockwise_up(self):
+        self.up_face = np.rot90(self.up_face, 1)
+        temp = self.back_face[-1].copy()
+        self.back_face[-1] = self.right_face[:,0]
+        self.right_face[:,0] = self.front_face[0][::-1]
+        self.front_face[0] = self.left_face[:,-1]
+        self.left_face[:,-1] = temp[::-1]
+
+    def anticlockwise_down(self):
+        self.down_face = np.rot90(self.down_face)
+        temp = self.back_face[0].copy()
+        self.back_face[0] = self.left_face[:,0][::-1]
+        self.left_face[:,0] = self.front_face[-1]
+        self.front_face[-1] = self.right_face[:,-1][::-1]
+        self.right_face[:,-1] = temp
---
+++ rubiksCube.py
@@ -0,0 +1,59 @@
+import numpy as np
+from tkinter import *
+import Cube
+
+class Window(Tk):
+    def __init__(self):
+        super().__init__()
+        self.cube = Cube.Cube(2)
+        self.title("Rubik's Cube")
+        self.geometry('500x400+700+100')
+        self.resizable(True, True)
+        self.canvas = Canvas(self, bg = '#191919', height = self.winfo_height(), width = self.winfo_width())
+        self.face_size = min(self.winfo_width() / 6, self.winfo_height() / 5)
+        self.draw_cube()
+        self.canvas.pack(fill = 'both', expand = True)  # these arguments make the canvas take up extra space when resizing the self
+
+        self.bind('u', lambda event: self.update_cube(self.cube.clockwise_up))  # lambda since passing function passes the return value not the actual function
+        self.bind('d', lambda event: self.update_cube(self.cube.clockwise_down))
+        self.bind('l', lambda event: self.update_cube(self.cube.clockwise_left))
+        self.bind('r', lambda event: self.update_cube(self.cube.clockwise_right))
+        self.bind('f', lambda event: self.update_cube(self.cube.clockwise_front))
+        self.bind('b', lambda event: self.update_cube(self.cube.clockwise_back))
+        self.bind('U', lambda event: self.update_cube(self.cube.anticlockwise_up))  # works with shift key
+        self.bind('D', lambda event: self.update_cube(self.cube.anticlockwise_down))
+        self.bind('L', lambda event: self.update_cube(self.cube.anticlockwise_left))
+        self.bind('R', lambda event: self.update_cube(self.cube.anticlockwise_right))
+        self.bind('F', lambda event: self.update_cube(self.cube.anticlockwise_front))
+        self.bind('B', lambda event: self.update_cube(self.cube.anticlockwise_back))
+        self.bind('<Configure>', lambda event: self.on_resize())
+    
+    def draw_cube(self):
+        self.draw_face(self.cube.left_face, [self.face_size, 2 * self.face_size])
+        self.draw_face(self.cube.back_face, [2 * self.face_size, self.face_size])
+        self.draw_face(self.cube.up_face, [2 * self.face_size, 2 * self.face_size])
+        self.draw_face(self.cube.front_face, [2 * self.face_size, 3 * self.face_size])
+        self.draw_face(self.cube.right_face, [3 * self.face_size, 2 * self.face_size])
+        self.draw_face(self.cube.down_face, [4 * self.face_size, 2 * self.face_size])
+    
+    def draw_face(self, face, coords):  # face will be a numpy array of integers. coords will start in top left corner
+        colour_dict = {0: "#FFFF00", 1: "#FFFFFF", 2: "#00FF00", 
+                    3: "#0000FF", 4: "#FFA500", 5: "#FF0000"}
+        for i in range(self.cube.dim):
+            for j in range(self.cube.dim):
+                x = coords[0] + i * (self.face_size / self.cube.dim)
+                y = coords[1] + j * (self.face_size / self.cube.dim)
+                self.canvas.create_rectangle(x,  y, x + (self.face_size / self.cube.dim), y + (self.face_size / self.cube.dim), fill = colour_dict[face[j][i]])
+    
+    def update_cube(self, rotation):
+        rotation()
+        self.draw_cube()
+
+    def on_resize(self):
+        self.face_size = min(self.winfo_width() / 6, self.winfo_height() / 5)
+        self.canvas.delete('all')
+        self.draw_cube()
+
+if __name__ == '__main__':
+    window = Window()
+    window.mainloop() # watches for events on tkinter objects
